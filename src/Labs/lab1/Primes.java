a82ca754d32bbaaec1f11700ad990ff2c3356ea8
IIcon18 <ogkrit882@gmail.com>
2024-09-19 21:10:57 +0300

    fix

--- src/Labs/lab1/Primes.java
+++ src/Labs/lab1/Primes.java
@@ -12,11 +12,9 @@ public class Primes {
     public static boolean isPrime(int n) {
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
-            if (n % i == 0) {
-                return false;
+            return false;
             }
         }
-    }
     return true;
     }
 }
