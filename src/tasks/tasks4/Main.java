76242ff7e287cd7e7e1ad36a506fa5e00717c111
IIcon18 <ogkrit882@gmail.com>
2024-10-25 20:58:45 +0300

    tasks4

---
+++ src/tasks/tasks4/Main.java
@@ -0,0 +1,50 @@
+package tasks.tasks4;
+
+public class Main {
+    public static void main(String[] args) {
+        //#1
+
+        //#2
+
+        //#3
+
+        //#4
+
+        //#5
+
+        //#6
+
+        //#7
+
+        //#8
+
+        //#9
+
+        //#10
+
+    }
+    //#1
+    public static String nonRepeat(String s) {
+
+    }
+
+    //#2
+
+    //#3
+
+    //#4
+
+    //#5
+
+    //#6
+
+    //#7
+
+    //#8
+
+    //#9
+
+    //#10
+
+    
+}
