571917dffedb76ea2d0e83d3fae6c6f6832e8ea8
IIcon18 <ogkrit882@gmail.com>
2024-10-31 14:01:16 +0300

    tasks4

--- src/tasks/tasks4/Main.java
+++ src/tasks/tasks4/Main.java
@@ -38,13 +38,16 @@ public class Main {
         System.out.println(uniqueSubstring("12223234333"));
         //#8
         System.out.println("#8");
-
+        System.out.println(labirint(new int[][]{{1, 3, 1}, {1, -1, 1}, {4, 2, 1}}));
+        System.out.println(labirint(new int[][]{{2, -7, 3}, {-4, -1, 8}, {4, 5, 9}}));
         //#9
         System.out.println("#9");
-
+        System.out.println(numericOrder("t3o the5m 1One all6 r4ule ri2ng") );
+        System.out.println(numericOrder("re6sponsibility Wit1h gr5eat power3 4comes g2reat"));
         //#10
         System.out.println("#10");
-
+        System.out.println(fibString("CCCABDD"));
+        System.out.println(fibString("ABC"));
     }
 
     //#1
@@ -207,10 +210,73 @@ public class Main {
         }
     }
     //#8
+    public static List<String> labirint(int[][] matrix) {
+        StringBuilder path = new StringBuilder();
+        int cost = findPath(matrix, 0, 0, path, 0);
+        if (cost == Integer.MAX_VALUE) {
+            return Collections.singletonList("Прохода нет");
+        }
+        path.deleteCharAt(path.length() - 1);
+        return Arrays.asList(path.toString(), String.valueOf(cost));
+    }
 
+    private static int findPath(int[][] matrix, int x, int y, StringBuilder path, int cost) {
+        int n = matrix.length;
+        if (x >= n || y >= n ||  matrix[x][y] < 0) {
+            return Integer.MAX_VALUE;
+        }
+        if (x == n - 1 && y == n - 1) {
+            path.append(matrix[x][y]).append("-");
+            return cost + matrix[x][y];
+        }
+        int temp = matrix[x][y];
+        matrix[x][y] = -1;
+        StringBuilder downPath = new StringBuilder(path);
+        StringBuilder rightPath = new StringBuilder(path);
+        int downCost = findPath(matrix, x + 1, y, downPath, cost + temp);
+        int rightCost = findPath(matrix, x, y + 1, rightPath, cost + temp);
+        matrix[x][y] = temp;
+        if (downCost < rightCost) {
+            path.setLength(0);
+            path.append(downPath).append(temp).append("-");
+            return downCost;
+        } else {
+            path.setLength(0);
+            path.append(rightPath).append(temp).append("-");
+            return rightCost;
+        }
+    }
     //#9
-
+    public static String numericOrder(String str) {
+        String[] words = str.split(" ");
+        String[] orderWords = new String[words.length];
+        for (String word : words) {
+            int num = Integer.parseInt(word.replaceAll("\\D", ""));
+            orderWords[num - 1] = word.replaceAll("\\d", "");
+        }
+        return String.join(" ", orderWords);
+    }
     //#10
+    public static boolean fibString(String str) {
+        Map<Character, Integer> countMap = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+        List<Integer> counts = new ArrayList<>(countMap.values());
+        Collections.sort(counts);
+        if (counts.size() < 3)
+            return false;
+        int fib1 = counts.get(0);
+        int fib2 = counts.get(1);
 
-    
+        for (int i = 2; i < counts.size(); i++) {
+            int fibNext = fib1 + fib2;
+            if (counts.get(i) != fibNext) {
+                return false;
+            }
+            fib1 = fib2;
+            fib2 = fibNext;
+        }
+        return true;
+    }
 }
